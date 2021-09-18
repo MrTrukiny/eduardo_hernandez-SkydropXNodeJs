408b478e95e0e184700ba0ca40b2d5660099f303
MrTrukiny <carloseduardo.tkn@gmail.com>
2021-09-18 18:01:32 -0500

    Create tests for saving user into DB

--- __tests__/users/userCreation.specs.js
+++ __tests__/users/userCreation.specs.js
@@ -14,7 +14,7 @@ describe('POST: User Creation', () => {
     expect(response.status).toBe(201);
   });
 
-  it("Returns success message when user creation is successful", async () => {
+  it('Returns success message when user creation is successful', async () => {
     const response = await await request(app).post('/users/1').send({
       email: 'testuser1@mail.com',
       first_name: 'Test User 1',
@@ -24,5 +24,38 @@ describe('POST: User Creation', () => {
       text: 'Test Description 1',
     });
     expect(response.body.message).toBe('User created successfully!');
+  });
+
+  it('Saves the user to database', async () => {
+    await request(app).post('/users/1').send({
+      email: 'testuser1@mail.com',
+      first_name: 'Test User 1',
+      last_name: 'Test Resu 1',
+      company: 'Test Company 1',
+      url: 'http://testurl.com',
+      text: 'Test Description 1',
+    });
+    // Query User
+    const usersList = await User.findAll();
+    expect(usersList.length).toBe(1);
+  });
+
+  it('Saves all the user info to database', async () => {
+    await request(app).post('/users/1').send({
+      email: 'testuser1@mail.com',
+      first_name: 'Test User 1',
+      last_name: 'Test Resu 1',
+      company: 'Test Company 1',
+      url: 'http://testurl.com',
+      text: 'Test Description 1',
+    });
+    const usersList = await User.findAll();
+    const savedUser = usersList[0];
+    expect(savedUser.email).toBe('testuser1@mail.com');
+    expect(savedUser.firstName).toBe('Test User 1');
+    expect(savedUser.lastName).toBe('Test Resu 1');
+    expect(savedUser.company).toBe('Test Company 1');
+    expect(savedUser.url).toBe('http://testurl.com');
+    expect(savedUser.text).toBe('Test Description 1');
   });
 });
