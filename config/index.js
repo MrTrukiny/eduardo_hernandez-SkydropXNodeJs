be04a4d4c5128dc9db61acde8d3e4fed9812eb6c
MrTrukiny <carloseduardo.tkn@gmail.com>
2021-09-28 03:25:26 -0500

    Create production config

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const development = require('./development');
 const staging = require('./staging');
 const test = require('./test');
-// const production = require('./production');
+const production = require('./production');
 
 module.exports = {
   nodeEnv: process.env.NODE_ENV,
   development,
   staging,
   test,
-  // production,
+  production,
 };
